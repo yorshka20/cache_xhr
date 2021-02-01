cd45d64de1d53aff7f1b51004d02f751cd7027db
yorshka <yorshka@youme.im>
2021-02-01 15:10:35 +0800

    请求缓存

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+
+import "./App.css";
+
+import CacheXHR from "./xhr";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const send = () => {
+    console.log("input", input);
+    const xhr = new CacheXHR(100);
+
+    const url = `http://${input}`;
+    xhr
+      .get(url)
+      .then((res) => {
+        console.log("res", res);
+        const container = document.getElementById("result");
+
+        container!.innerText = res + "\n";
+      })
+      .catch((err) => {
+        console.log("err", err);
+        const container = document.getElementById("result");
+
+        container!.innerText = "request error" + "\n";
+      });
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+
+    setInput(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Cache xhr test</header>
+
+      <div className="inputWrapper">
+        <input onChange={handleChange} placeholder="url" type="text" />
+        <button onClick={send}>send</button>
+      </div>
+
+      <div className="result" id="result"></div>
     </div>
   );
 }
--- src/xhr.ts
+++ src/xhr.ts
@@ -2,7 +2,7 @@
  * @Author: yorshka
  * @Date: 2021-02-01 13:34:26
  * @Last Modified by: yorshka
- * @Last Modified time: 2021-02-01 13:39:27
+ * @Last Modified time: 2021-02-01 15:09:33
  */
 
 interface CacheItem {
@@ -10,19 +10,15 @@ interface CacheItem {
   result: any;
 }
 
-interface RequestQueueItem {
-  id: string;
-  xhr: XMLHttpRequest;
-}
-
 export default class CacheXHR {
   static instance: CacheXHR;
+
   // 缓存容量
-  cacheLengthLimit = 0;
+  private cacheLengthLimit = 0;
   // 缓存bucket
-  cachePool: Array<CacheItem> = [];
+  private cachePool: Array<CacheItem> = [];
   // 请求队列
-  requestQueue: Array<RequestQueueItem> = [];
+  private requestQueue: Map<string, XMLHttpRequest> | undefined;
 
   constructor(length: number) {
     if (CacheXHR.instance) {
@@ -30,7 +26,7 @@ export default class CacheXHR {
     }
 
     this.cachePool = [];
-    this.requestQueue = [];
+    this.requestQueue = new Map<string, XMLHttpRequest>();
     this.cacheLengthLimit = length;
 
     CacheXHR.instance = this;
@@ -38,11 +34,13 @@ export default class CacheXHR {
 
   public get(url: string) {
     return new Promise((resolve, reject) => {
+      const id = `get:${url}`;
+
       // 检查缓存中是否存在结果
       let hasCache = false;
       let result;
       for (let cache of this.cachePool) {
-        if (cache.id === `get:${url}`) {
+        if (cache.id === id) {
           hasCache = true;
           result = cache;
         }
@@ -50,38 +48,59 @@ export default class CacheXHR {
 
       // 有缓存
       if (hasCache && result) {
-        return resolve(result);
+        return resolve(result.result);
       }
 
       // 检测是否正在并发请求
       let isRequesting = false;
       let requestHandler;
-      for (let request of this.requestQueue) {
-        if (request.id === `get:${url}`) {
-          isRequesting = true;
-          requestHandler = request.xhr;
+      const pendingRequest = this.requestQueue?.get(id);
+      if (pendingRequest) {
+        isRequesting = true;
+        requestHandler = pendingRequest;
+      }
+
+      // 若存在请求，则等待当前请求的onload
+      if (isRequesting && requestHandler) {
+        requestHandler.onload = (e: any) => {
+          const responseText = e!.currentTarget?.responseText;
+          return resolve(responseText);
+        };
+      } else {
+        // 没有缓存，新建请求
+        const xhr = new XMLHttpRequest();
+        // 请求入队列
+        this.requestQueue!.set(id, xhr);
+
+        xhr.open("get", url);
+        xhr.onload = (e: ProgressEvent) => {
+          // 检查缓存容量
+          if (this.cacheLengthLimit < this.cachePool.length) {
+            this.cachePool.shift();
+          }
+
+          // 加入缓存
+          this.cachePool.push({
+            id,
+            result: xhr.responseText,
+          });
+
+          // 从请求队列中清除
+          this.requestQueue?.delete(id);
+
+          return resolve(xhr.responseText);
+        };
+
+        xhr.onerror = (err: any) => {
+          return reject(err);
+        };
+
+        try {
+          xhr.send();
+        } catch (error) {
+          reject(error);
         }
       }
-
-      if (isRequesting && requestHandler) {
-        requestHandler.addEventListener("cached", (result) => {
-          return resolve(result);
-        });
-      }
-
-      const xhr = new XMLHttpRequest();
-
-      xhr.open("get", url);
-      xhr.send();
-    });
-  }
-
-  post(url: string, data = {}) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-
-      xhr.open("post", url);
-      xhr.send(data as FormData);
     });
   }
 }
